5ac695518749487bad2a471773745ee73069f442
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-09-25 10:03:17 +0600

    done cursor animation

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let cursor = document.querySelector('#cursor');
+let cursorBlur = document.querySelector('#cursor-blur');
+document.addEventListener('mousemove', function (e) {
+    // cursor.style.left=e.x+'px'
+    // cursor.style.top=e.y+'px'
+    gsap.to(cursor, {
+        x: e.x,
+        y: e.y,
+        duration: 1,
+        ease: 'back.out'
+    })
+})
+document.addEventListener('mousemove', function (e) {
+    // cursor.style.left=e.x+'px'
+    // cursor.style.top=e.y+'px'
+    gsap.to(cursorBlur, {
+        x: e.x-200,
+        y: e.y-200,
+        duration: 1,
+        ease: 'back.out'
+    })
+})
+
+
 gsap.to('#nav', {
     backgroundColor: '#000',
     duration: 0.5,
@@ -17,7 +41,7 @@ gsap.to('#main', {
     scrollTrigger: {
         trigger: '#main',
         scroller: 'body',
-        markers: true,
+        // markers: true,
         start:'top -25%',
         end: 'top -70%',
         scrub:2
