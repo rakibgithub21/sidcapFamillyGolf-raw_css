0488951a9c8d0fee320b0c96b96dccfcde5607b6
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-09-25 09:48:29 +0600

    add another gsap for change background colot t

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ gsap.to('#nav', {
     scrollTrigger: {
         trigger: '#nav',
         scroll: 'body',
-        markers: true,
         start: 'top -10%',
         end: 'top -11%',
         scrub: 1,
         
    }
+})
+
+gsap.to('#main', {
+    backgroundColor: '#000',
+    scrollTrigger: {
+        trigger: '#main',
+        scroller: 'body',
+        markers: true,
+        start:'top -25%',
+        end: 'top -70%',
+        scrub:2
+    }
 })
